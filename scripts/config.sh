d4f84763640b83fab98517bf29c5033ebcc44364
Cmer <cmer@cmer.fr>
2016-02-06 15:05:58 +0100

    modified:   scripts/config.sh modified:   scripts/dns-start

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,10 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+echo $DNS_IP_MASTER
 
 echo 'controls {
-    inet * port 953 allow { $DNS_IP_MASTER; 127.0.0.1; } keys { "rndc-key"; "rndc-key-$DNS_IP_MASTER"; };
+    inet * port 953 allow { '$DNS_IP_MASTER'; 127.0.0.1; } keys { "rndc-key"; "rndc-key-'$DNS_IP_MASTER'"; };
 };' >> /etc/bind/named.conf.local
 
-echo 'key "rndc-key-$DNS_IP_MASTER" {
+echo 'key "rndc-key-'$DNS_IP_MASTER'" {
   algorithm hmac-md5;
-  secret "$RNDC_KEY";
+  secret "'$RNDC_KEY'";
 };' >> /etc/bind/rndc.key
